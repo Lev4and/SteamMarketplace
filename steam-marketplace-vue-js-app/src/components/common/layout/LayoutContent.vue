19b31a4bdba11211d4e09fe223b25f6bfe88b9f4
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-05-16 23:35:02 +0500

    Отрефакторил компонент "App". Импортировал иконки.

---
+++ steam-marketplace-vue-js-app/src/components/common/layout/LayoutContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-layout class="layout-content">
+    <a-breadcrumb style="margin: 16px 0">
+      <a-breadcrumb-item>Главная</a-breadcrumb-item>
+    </a-breadcrumb>
+    <a-layout-content class="content" >
+      <router-view />
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<script>
+  export default {
+    name: 'LayoutContent',
+  }
+</script>
+
+<style scoped>
+  .layout-content {
+    padding: 0 24px 24px;
+  }
+  .layout-content .content {
+    margin: 0;
+    padding: 24px;
+    min-height: 280px;
+    background: #fff;
+  }
+</style>
---
+++ steam-marketplace-vue-js-app/src/components/common/layout/LayoutSider.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-layout-sider class="sider">
+    <a-menu
+      mode="inline"
+      :default-open-keys="['database']"
+      :style="{ height: '100%', borderRight: 0 }"
+    >
+      <a-sub-menu key="database">
+        <span slot="title"><a-icon type="database" /> База данных</span>
+      </a-sub-menu>
+    </a-menu>
+  </a-layout-sider>
+</template>
+
+<script>
+  export default {
+    name: 'LayoutSider',
+  }
+</script>
+
+<style scoped>
+  .sider {
+    width: 200px;
+    background: #fff;
+  }
+</style>
