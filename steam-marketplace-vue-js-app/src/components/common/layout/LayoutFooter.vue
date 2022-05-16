f768498171aebbf4f8850d9acf27c816c4d22052
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-05-16 23:49:12 +0500

    Добавил Footer. Исправил стили.

---
+++ steam-marketplace-vue-js-app/src/components/common/layout/LayoutFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center' }">
+    <a href="https://github.com/Lev4and/SteamMarketplace">SteamMarketplace</a> ©2022 Разработан <a href="https://github.com/Lev4and">Lev4and</a>
+  </a-layout-footer>
+</template>
+
+<script>
+  export default {
+    name: 'LayoutFooter',
+  }
+</script>
+
+<style scoped>
+
+</style>
