724cd7cf83b2f10d7f135f3e92837c7ffc50af6f
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-05-23 23:28:54 +0500

    Реализована страница просмотра всех покупок пользователя.

--- steam-marketplace-vue-js-app/src/components/purchases/MyPurchase.vue
+++ steam-marketplace-vue-js-app/src/components/purchases/MyPurchase.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="cs-money-item">
+  <div class="my-purchase">
     <a-row :gutter="[0,12]">
       <a-col :span="24" class="image-container">
-        <img :src="item.steamImg" />
+        <img :src="item.image.steamImg" />
       </a-col>
       <a-col :span="24">
         <a-tooltip>
@@ -21,45 +21,49 @@
         </a-tooltip>
       </a-col>
       <a-col :span="24">
+        <div class="purchase-info">
+          <div class="seller">
+            <a-tooltip>
+              <template slot="title">
+                <span>Продавец: {{ purchase.sale.seller.userName }}</span>
+              </template>
+              <a-icon type="user" />
+              <span class="seller-title" v-text="purchase.sale.seller.userName" />
+            </a-tooltip>
+          </div>
+          <div class="purchase-at">
+            <a-tooltip>
+              <template slot="title">
+                <span>Куплено в: {{ purchase.purchaseAt }}</span>
+              </template>
+              <a-icon type="clock-circle" />
+              <span class="purchase-at-time" v-text="purchase.purchaseAt" />
+            </a-tooltip>
+          </div>
+        </div>
+      </a-col>
+      <a-col :span="24">
         <a-tooltip>
           <template slot="title">
-            <span style="font-size: 20px;">{{ currentPrice }}
-              <sup v-if="overprice !== 1" class="original-price">
-                <small v-text="originalPrice" />
-              </sup>
+            <span style="font-size: 20px;" v-text="priceFormat">
             </span>
           </template>
-          <strong class="actual-price" v-text="currentPrice" />
+          <strong class="actual-price" v-text="priceFormat" />
         </a-tooltip>
       </a-col>
     </a-row>
-    <div class="stack-container">
-      <a-badge :count="stackSize" />
-    </div>
-    <div class="stickers-container">
-      <template v-for="(sticker, i) in stickers">
-        <div :key="i" class="sticker">
-          <a-tooltip>
-            <template slot="title">
-              {{ sticker.name }}
-            </template>
-            <img :src="sticker.img" class="sticker-image" />
-          </a-tooltip>
-        </div>
-      </template>
-    </div>
   </div>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
-  import { first as _first, filter as _filter, join as _join } from 'lodash'
+  import { first as _first, join as _join, filter as _filter } from 'lodash'
 
   export default {
-    name: 'CSMoneyItem',
+    name: 'MyPurchase',
 
     props: {
-      item: {
+      purchase: {
         type: Object,
         required: true,
       },
@@ -79,34 +83,24 @@
         return this.currentUser?.currency?.cultureInfoName || 'us-US'
       },
       price() {
-        return this.item.price
+        return this.purchase.price
       },
-      overprice() {
-        return (100 + (this.item.overprice || 0)) / 100
-      },
-      currentPrice() {
+      priceFormat() {
         return new Intl.NumberFormat(this.cultureInfoName, { style: 'currency', currency: this.currency })
           .format(this.price * this.exchangeRate)
       },
-      originalPrice() {
-        return new Intl.NumberFormat(this.cultureInfoName, { style: 'currency', currency: this.currency })
-          .format(this.price * this.overprice * this.exchangeRate)
-      },
-      stickers() {
-        return _filter(this.item.stickers || [], (sticker) => sticker)
-      },
-      stackSize() {
-        return this.item.stackSize || 0
+      item() {
+        return this.purchase.sale.item
       },
       float() {
         return this.item.float ? new Intl.NumberFormat(this.cultureInfoName, { maximumSignificantDigits: 6 })
           .format(this.item.float) : ''
       },
       rarity() {
-        return this.item.rarity?.toUpperCase() || ''
+        return this.item.rarity?.name?.toUpperCase() || ''
       },
       quality() {
-        return this.item.quality?.toUpperCase() || ''
+        return this.item.quality?.name?.toUpperCase() || ''
       },
       info() {
         return _join(_filter([this.rarity, this.quality, this.float], (info) => info), ' / ') || 'Нет информации'
@@ -116,7 +110,7 @@
 </script>
 
 <style scoped>
-  .cs-money-item {
+  .my-purchase {
     padding: 10px;
     border-radius: 10px;
     border: 1px solid black;
@@ -153,49 +147,31 @@
     -webkit-line-clamp: 1;
     -webkit-box-orient: vertical;
   }
+  .purchase-info {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .purchase-info .seller,
+  .purchase-info .purchase-at {
+    width: 45%;
+    font-size: 12px;
+    word-break: break-all;
+    flex-grow: 0;
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+  }
+  .seller .seller-title,
+  .purchase-at .purchase-at-time {
+    margin-left: 5px;
+  }
   .actual-price {
     display: block;
     font-size: 20px;
-    text-align: left;
+    text-align: center;
     color: rgb(249, 17, 85);
-  }
-  .original-price::after {
-    height: 3px;
-    content: "";
-    display: block;
-    left: 0;
-    position: absolute;
-    top: calc(50% - 1px);
-    transform: rotate(-2deg);
-    background-color: red;
-    width: 100%;
-  }
-  .stack-container {
-    top: 15px;
-    bottom: 0;
-    left: 10px;
-    width: 35px;
-    display: flex;
-    position: absolute;
-    flex-direction: column;
-  }
-  .stickers-container {
-    top: 10px;
-    bottom: 0;
-    right: 10px;
-    width: 35px;
-    display: flex;
-    position: absolute;
-    flex-direction: column;
-  }
-  .stickers-container .sticker {
-    width: 100%;
-    height: 35px;
-    display: flex;
-  }
-  .sticker img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
   }
 </style>
---
+++ steam-marketplace-vue-js-app/src/views/Purchases.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="purchases">
+    <my-purchases />
+  </div>
+</template>
+
+<script>
+  import MyPurchases from '@/components/purchases/MyPurchases'
+
+  export default {
+    name: 'Purchases',
+
+    components: {
+      MyPurchases,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
