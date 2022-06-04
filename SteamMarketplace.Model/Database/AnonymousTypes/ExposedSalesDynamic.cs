602f5d0cf219d1dbe4fa51e0df908377b8740e41
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-06-04 17:35:04 +0500

    Реализовал запрос для получения динамики выставления на продажу определенного товара.

---
+++ SteamMarketplace.Model/Database/AnonymousTypes/ExposedSalesDynamic.cs
@@ -0,0 +1,11 @@
+﻿namespace SteamMarketplace.Model.Database.AnonymousTypes
+{
+    public class ExposedSalesDynamic
+    {
+        public int CountExposed { get; set; }
+
+        public decimal MinPriceUsd { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
