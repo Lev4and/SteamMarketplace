2412cc688675e284d21c7bdfdc27917518750473
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-06-04 11:30:12 +0500

    Реализовано получение количества и списка пользователей в онлайне через SignalR.

--- SteamMarketplace.ResourceWebApplication/Hubs/OnlineHub.cs
+++ SteamMarketplace.ResourceWebApplication/Hubs/OnlineHub.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
+using SteamMarketplace.Model.Database.Entities;
 
 namespace SteamMarketplace.ResourceWebApplication.Hubs
 {
@@ -7,7 +7,7 @@ namespace SteamMarketplace.ResourceWebApplication.Hubs
     {
         private readonly ILogger<OnlineHub> _logger;
 
-        static HashSet<string> Connections = new HashSet<string>();
+        static Dictionary<string, ApplicationUser> Connections = new Dictionary<string, ApplicationUser>();
 
         public OnlineHub(ILogger<OnlineHub> logger)
         {
@@ -16,23 +16,47 @@ namespace SteamMarketplace.ResourceWebApplication.Hubs
 
         public override async Task OnConnectedAsync()
         {
-            Connections.Add(Context.ConnectionId);
+            Connections.Add(Context.ConnectionId, null);
 
             _logger.LogInformation($"Microsoft.AspNetCore.SignalR OnlineHub New connection {Context.ConnectionId} " +
                 $"Current online {Connections.Count}");
 
-            await Clients.All.SendAsync("UserConnected", Connections.Count);
+            await Clients.All.SendAsync("OnlineChanged", Connections.Count);
+
             await base.OnConnectedAsync();
+        }
+
+        public async Task MatchUser(ApplicationUser user)
+        {
+            Connections[Context.ConnectionId] = user;
+
+            _logger.LogInformation($"Microsoft.AspNetCore.SignalR OnlineHub The user {user.UserName} has been matched with " +
+                $"connection {Context.ConnectionId}. Current online {Connections.Count}");
+
+            await Clients.All.SendAsync("UserConnected", user);
+            await Clients.All.SendAsync("OnlineChanged", Connections.Count);
+        }
+
+        public async Task GetUsers()
+        {
+            _logger.LogInformation($"Microsoft.AspNetCore.SignalR OnlineHub The connection {Context.ConnectionId} " +
+                $"requested a list of online users.");
+
+            await Clients.Caller.SendAsync("OnlineUsers", Connections.Values);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            var connection = Connections[Context.ConnectionId];
+
             Connections.Remove(Context.ConnectionId);
 
             _logger.LogInformation($"Microsoft.AspNetCore.SignalR OnlineHub Lose connection {Context.ConnectionId} " +
                 $"Current online {Connections.Count} Reason {exception?.Message}");
 
-            await Clients.All.SendAsync("UserDisconnected", Connections.Count);
+            await Clients.All.SendAsync("UserDisconnected", connection);
+            await Clients.All.SendAsync("OnlineChanged", Connections.Count);
+
             await base.OnDisconnectedAsync(exception);
         }
     }
