c85701e79f1f6efaa9ac553eb8d70af3d0b9fdb8
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-06-13 23:32:39 +0500

    Реализовал подгрузку вложенных элементов.

---
+++ SteamMarketplace.ResourceWebApplication/Controllers/ItemNestedsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SteamMarketplace.Model.Common;
+using SteamMarketplace.Model.Database;
+using SteamMarketplace.Model.Database.Entities;
+
+namespace SteamMarketplace.ResourceWebApplication.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/itemNesteds")]
+    [EnableCors("CorsPolicy")]
+    public class ItemNestedsController : Controller
+    {
+        private readonly ILogger<ItemNestedsController> _logger;
+        private readonly DefaultDataManager _dataManager;
+
+        public ItemNestedsController(DefaultDataManager dataManager, ILogger<ItemNestedsController> logger)
+        {
+            _logger = logger;
+            _dataManager = dataManager;
+        }
+
+        [HttpPost]
+        [Route("byItemIds")]
+        public async Task<IActionResult> GetItemNesteds([FromBody] List<Guid> itemIds)
+        {
+            if (itemIds == null)
+            {
+                _logger.LogWarning($"Validation failed. Invalid filters param.");
+
+                return BadRequest(new BaseResponseModel<object?>(null, Statuses.InvalidData));
+            }
+
+            return Ok(new BaseResponseModel<List<ItemNested>>(await _dataManager.ItemNesteds.GetItemNesteds(itemIds)
+                .ToListAsync(), Statuses.Success));
+        }
+    }
+}
