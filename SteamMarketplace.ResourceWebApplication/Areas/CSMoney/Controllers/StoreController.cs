69adc4396d0246fa1cf8cdb2f5e328e67cc6f993
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-06-14 23:17:45 +0500

    Реализовал при получении скинов из магазина CS.Money возвращение модель данных не BaseResponseModel, а PagedResponseModel.

--- SteamMarketplace.ResourceWebApplication/Areas/CSMoney/Controllers/StoreController.cs
+++ SteamMarketplace.ResourceWebApplication/Areas/CSMoney/Controllers/StoreController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using SteamMarketplace.HttpClients.CSMoney.ResponseModels;
 using SteamMarketplace.Model.Common;
+using SteamMarketplace.Model.Marketplace.CSMoney.Types;
 using System.ComponentModel.DataAnnotations;
 
 namespace SteamMarketplace.ResourceWebApplication.Areas.CSMoney.Controllers
@@ -25,26 +26,25 @@ namespace SteamMarketplace.ResourceWebApplication.Areas.CSMoney.Controllers
 
         [HttpGet]
         [Route("inventory")]
-        [ProducesResponseType(typeof(BaseResponseModel<object?>), 401)]
-        [ProducesResponseType(typeof(BaseResponseModel<BotInventory>), 200)]
-        [ProducesResponseType(typeof(BaseResponseModel<BotInventory>), 400)]
         public async Task<IActionResult> GetInventory([Required][FromQuery(Name = "limit")] int limit, [Required][FromQuery(Name = "offset")] int offset, [FromQuery(Name = "withStack")] bool withStack = true)
         {
             if (limit <= 0)
             {
                 _logger.LogWarning($"Validation failed. Invalid limit param.");
 
-                return BadRequest(new BaseResponseModel<BotInventory>(new BotInventory(), Statuses.InvalidData));
+                return BadRequest(new PagedResponseModel<Item>(new List<Item>(), 1, 0, 0, Statuses.InvalidData));
             }
 
             if (offset < 0)
             {
                 _logger.LogWarning($"Validation failed. Invalid offset param.");
 
-                return BadRequest(new BaseResponseModel<BotInventory>(new BotInventory(), Statuses.InvalidData));
+                return BadRequest(new PagedResponseModel<Item>(new List<Item>(), 1, 0, 0, Statuses.InvalidData));
             }
 
-            return Ok(new BaseResponseModel<BotInventory>(await _httpContext.CSMoney.Store.GetInventoryAsync(limit, offset, withStack),
+            var botInventory = await _httpContext.CSMoney.Store.GetInventoryAsync(limit, offset, withStack);
+
+            return Ok(new PagedResponseModel<Item>(botInventory.Items, offset / limit + 1, limit, 150000, 
                 Statuses.Success));
         }
     }
