7a14f20c0a569be2fa5ebb98a78c75a70ad7817e
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-06-18 22:16:30 +0500

    Реализованы все CRUD операции над сущностью 'Currency'.

--- SteamMarketplace.Model/Database/Repositories/ObjectRelational/Abstract/ICurrenciesRepository.cs
+++ SteamMarketplace.Model/Database/Repositories/ObjectRelational/Abstract/ICurrenciesRepository.cs
@@ -1,8 +1,9 @@
-﻿using SteamMarketplace.Model.Database.Entities;
+﻿using SteamMarketplace.Model.Database.AuxiliaryTypes;
+using SteamMarketplace.Model.Database.Entities;
 
 namespace SteamMarketplace.Model.Database.Repositories.ObjectRelational.Abstract
 {
-    public interface ICurrenciesRepository
+    public interface ICurrenciesRepository : IFilterableRepository<Currency, CurrenciesFilters>, ICRUDRepository<Currency>
     {
         IQueryable<Currency> GetAllCurrencies();
     }
