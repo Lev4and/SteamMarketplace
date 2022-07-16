43c475d8b1a47ab3cba06c7109d35eddcaa704ff
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-07-16 23:36:37 +0500

    Изменил скрипт инициализации traffic.

--- traefikInit.sh
+++ traefikInit.sh
@@ -3,5 +3,3 @@ touch data/acme.json
 chmod 600 data/acme.json
 cp traefik.yml data/configurations/traefik.yml
 cp dynamic.yml data/configurations/dynamic.yml
-sudo cp ../acme.json ./
-sudo cp acme.json data/
