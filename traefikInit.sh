e1253c8b8de8dc0592b2555dbe7223b79bd4d667
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-07-16 23:30:11 +0500

    Создал файлы для traefik.

---
+++ traefikInit.sh
@@ -0,0 +1,7 @@
+mkdir -p data/configurations
+touch data/acme.json
+chmod 600 data/acme.json
+cp traefik.yml /data/configurations/traefik.yml
+cp dynamic.yml /data/configurations/dynamic.yml
+sudo cp ../acme.json ./
+sudo cp acme.json data/
