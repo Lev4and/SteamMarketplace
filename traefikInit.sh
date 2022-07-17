76e223f25092b4e8cf484b9f819cd17360f72efd
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-07-17 08:28:36 +0500

    Обновил файл traefikInit.sh

--- traefikInit.sh
+++ traefikInit.sh
@@ -1,5 +1,5 @@
 mkdir -p data/configurations
 touch data/acme.json
 chmod 600 data/acme.json
-cp traefik.yml data/configurations/traefik.yml
-cp dynamic.yml data/configurations/dynamic.yml
+cp traefik.yml data/configurations
+cp dynamic.yml data/configurations
