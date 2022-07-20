36b638b4c22fb1539ea878eee9b4a46082b9a0ab
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-07-20 19:44:20 +0500

    Сменил URL-ы для API. Дополнил origins для CORS-policy.

--- steam-marketplace-vue-js-app/src/api/config.js
+++ steam-marketplace-vue-js-app/src/api/config.js
@@ -1,2 +1,2 @@
-export const resourceAPIUrl = 'http://localhost/resource'
-export const authorizationAPIUrl = 'http://localhost/authorization'
+export const resourceAPIUrl = 'http://194-67-67-175.cloudvps.regruhosting.ru/resource'
+export const authorizationAPIUrl = 'http://194-67-67-175.cloudvps.regruhosting.ru/authorization'
