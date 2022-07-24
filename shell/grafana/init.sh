e5d85f99f94f1f134af4eeb0baacff239ead3f76
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-07-24 13:56:52 +0500

    Обновил docker-compose. Изменил конфигурацию nginx. Добавил памятку для инициализации grafana. Изменил конфигурационный файл prometheus.

---
+++ shell/grafana/init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#!/usr/bin/env bash
+which sh
+ip a
+# Open Grafana Dashboard
+# Click on "Data Sources"
+# Click on "Prometheus"
+# Write to URL input: http://ip_address:prometheus_port
+# Click on "Save & test"
+# Select "Import"
+# https://grafana.com/grafana/dashboards/1860
+# Write to Import via grafana.com input: 1860
+# Click on "Load"
+# Select Prometheus
